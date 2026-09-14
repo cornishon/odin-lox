c413f20e4e333de25330078a7fa5699512027554
Adam Z <zadam@tutamail.com>
2026-09-14 15:56:56 +0200

    Add examples folder

---
+++ examples/fib.py
@@ -0,0 +1,10 @@
+# for comparison with fib.lox
+import time
+
+def fib(n):
+    if n < 2: return n
+    return fib(n - 2) + fib(n - 1)
+
+start = time.perf_counter()
+print(fib(35))
+print(time.perf_counter() - start)
